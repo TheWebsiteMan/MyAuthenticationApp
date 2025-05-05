97bbd0f3f95220cace1cbef41e9525dc3128f84e
Xavier P <126206539+TheWebsiteMan@users.noreply.github.com>
2025-05-05 08:52:44 +0000

    general fixes

--- main.py
+++ main.py
@@ -2,104 +2,89 @@ import bcrypt
 
 # Initialise global vars
 salt = b"$2b$12$ieYNkQp8QumgedUo30nuPO"
-updatelogin = 0
 current_user = ""
+
+def load_users():
+    users = {}
+    with open("plain_text.txt", "r") as file:
+        for line in file:
+            user, password = line.strip().split(",")
+            users[user] = password
+    return users
 
 def login():
     global current_user
     while True:
         user = input("Username: ")
         password = input("Password: ")
-        hashed_password = bcrypt.hashpw(password.encode(), salt=salt)
-        with open("plain_text.txt", "r") as file:
-            for line in file: 
-                user_t, input_password = line.rstrip().split(",")
-                if user == user_t and bcrypt.checkpw(input_password.encode(), hashed_password):
-                    current_user = user
-                    print(f"Welcome, {user}!")
-                    return 
-            print("Incorrect username or password.")
+        byte_string = load_users()[user]
+        byte_string = byte_string.encode('utf-8')
+        if bcrypt.checkpw(password.encode(), byte_string):
+            print("Login successful!")
+            current_user = user
+            print(f"Welcome, {user}!")
+            return
+        else:
+            print("Incorrect username or password")
 
-def register():
-    while True:
-        user = input("Username: ")
-        password = input("Password: ")
-        with open("plain_text.txt", "r") as file:
-            lines = file.readlines()
-            for line in lines: 
-                user_t, input_password = line.rstrip().split(",")
-                if user != user_t:
-                    if len(password) > 3:
-                        with open("plain_text.txt", "a") as file:
-                            file.write("\n")
-                            file.write(f"{user},{password}")
-                        print("Registered!")
-                        return 
-                    else:
-                        print("Error: Password must be at least 4 characters")
-                else:
-                    print("Error: Username already taken")
-                    # code doesnt work because the not user_t only checks once
+def register(users):
+    user = input("Username: ").strip()
+    if user in users:
+        print("Error: Username already taken")
+        return
+    password = input("Password: ").strip()
+    if len(password) < 4:
+        print("Error: Password must be at least 4 characters")
+        return
+    users[user] = bcrypt.hashpw(password.encode(), salt=salt)
+    with open("plain_text.txt", "a") as file:
+        file.write(f"{user},{users[user]}\n")
+    print("Registered!")
 
-def changepass():
+def changepass(users):
     global current_user
-    with open("plain_text.txt", "r") as file:
-        lines = file.readlines()
-        for line in lines:
-            user_t, input_password = line.rstrip().split(",")
-            if current_user == user_t:
-                new_pass = input("New password: ")
-                if len(new_pass) > 3:
-                    with open("plain_text.txt", "a") as file:
-                        file.write("\n")
-                        file.write(f"{user_t},{new_pass}")
-                        #remove old password
-                else:
-                    print("Error: Password must be at least 4 characters")
-        
-
-
-def menu_logic():
-    global updatelogin
-    if updatelogin == 0:
-        print("1. Login")
-        print("2. Register")
-        print("3. Quit")
-        l1 = input(": ")
-        match l1:
-            case "1":
-                login()
-                updatelogin = 1
-                return
-            case "2":
-                register()
-                return
-            case "3":
-                print("Goodbye!")
-                return 7
-            case _:
-                return
-    else:
-        print("1: Change password")
-        print("2: Logout")
-        l2 = input(": ")
-        match l2:
-            case "1":
-                changepass()
-                return
-            case "2":
-                print("Goodbye!")
-                return 7
-            case _:
-                return
-
-def menu():
-    while True:
-        if menu_logic() == 7:
-            break
-
+    if current_user not in users:
+        print("Error: User not logged in")
+        return
+    new_pass = input("New password: ").strip()
+    if len(new_pass) < 4:
+        print("Error: Password must be at least 4 characters")
+        return
+    users[current_user] = bcrypt.hashpw(new_pass, salt=salt)
+    with open("plain_text.txt", "w") as file:
+        for user, password in users.items():
+            file.write(f"{user},{password}\n")
+    print("Password updated!")
 
 def main():
-    menu()
+    global current_user
+    while True:
+        if current_user == "":
+            print("1. Login")
+            print("2. Register")
+            print("3. Quit")
+            l1 = input(": ")
+            match l1:
+                case "1":
+                    login()
+                case "2":
+                    register(load_users())
+                case "3":
+                    print("Goodbye!")
+                    break
+                case _:
+                    print()
+        else:
+            print("1: Change password")
+            print("2: Logout")
+            l2 = input(": ")
+            match l2:
+                case "1":
+                    changepass(load_users())
+                case "2":
+                    print("Goodbye!")
+                    current_user = ""
+                case _:
+                    print()
 
 main()
